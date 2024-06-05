8501bcde7d7359297ed028841ecbf09b2f02d89b
kito <surya.aw26@gmail.com>
2024-06-05 12:04:36 +0700

    refactor codes

--- js/utang.js
+++ js/utang.js
@@ -10,17 +10,13 @@ function tampilUtang() {
             let temp = '';
 
             res.data.forEach((d) => {
-                const jml_transaksi = rupiahFormatter(d.jml_transaksi)
-                const jml_dibayar = rupiahFormatter(d.jml_dibayar)
-                const jml_kekurangan = rupiahFormatter(d.jml_kekurangan)
-
                 temp += `
                     <tr>
-                        <td class="label-cell" style="width: 40%">${d.suplier_nama}</td>
-                        <td class="numeric-cell" style="width: 30%">${d.tgl_transaksi}</td>
-                        <td class="numeric-cell" style="width: 20%">${jml_transaksi}</td>
-                        <td class="numeric-cell" style="width: 20%">${jml_dibayar}</td>
-                        <td class="numeric-cell" style="width: 20%">${jml_kekurangan}</td>
+                        <td class="label-cell">${d.suplier_nama}</td>
+                        <td class="numeric-cell">${d.tgl_transaksi}</td>
+                        <td class="numeric-cell">${rupiahFormatter(d.jml_transaksi)}</td>
+                        <td class="numeric-cell">${rupiahFormatter(d.jml_dibayar)}</td>
+                        <td class="numeric-cell">${rupiahFormatter(d.jml_kekurangan)}</td>
                         <td class="label-cell"><button class="button button-small button-tonal" onclick="detailUtang(${d.id})">Lihat</button></td>
                     </tr>
                 `
