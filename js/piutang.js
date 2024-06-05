ef85401904ad2f7ccbee8c89c85db6557fe1134c
kito <surya.aw26@gmail.com>
2024-06-06 06:19:21 +0700

    add rupiah formatter in piutang

--- js/piutang.js
+++ js/piutang.js
@@ -12,15 +12,16 @@ function tampilPiutang() {
             res.data.forEach((d) => {
                 temp += `
                     <tr>
-                        <td class="label-cell" style="width: 40%">${d.pelanggan_nama}</td>
-                        <td class="numeric-cell" style="width: 30%">${d.tgl_transaksi}</td>
-                        <td class="numeric-cell" style="width: 20%">${d.jml_transaksi}</td>
-                        <td class="numeric-cell" style="width: 20%">${d.jml_dibayar}</td>
-                        <td class="numeric-cell" style="width: 20%">${d.jml_kekurangan}</td>
+                        <td class="label-cell">${d.pelanggan_nama}</td>
+                        <td class="numeric-cell">${d.tgl_transaksi}</td>
+                        <td class="numeric-cell">${rupiahFormatter(d.jml_transaksi)}</td>
+                        <td class="numeric-cell">${rupiahFormatter(d.jml_dibayar)}</td>
+                        <td class="numeric-cell">${rupiahFormatter(d.jml_kekurangan)}</td>
                         <td class="label-cell"><button class="button button-small button-tonal" onclick="#">Lihat</button></td>
                     </tr>
                 `
             })
+
             $('#daftar-piutang').html(temp)
         }
     })
